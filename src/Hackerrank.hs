c4ba49ec96d7f74f89916c63061f6baa88d692e3
Arnav Sastry <arnavsastry@gmail.com>
2015-12-20 22:53:21 -0600

    regurgitating HackerRank API

---
+++ src/Hackerrank.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Hackerrank where
+
+import Control.Lens
+
+import Data.Map as Map
+import Data.Aeson
+import Data.Aeson.Lens (key)
+
+import Network.Wreq
+
+type Resp = Response (Map String Value)
+
+languagesUrl = "http://api.hackerrank.com/checker/languages.json"
+
+languages :: IO (Maybe Value)
+languages = do
+  r <- asValue =<< get languagesUrl
+  return (r ^? responseBody . key "languages" . key "names")
--- src/UTPC.hs
+++ src/UTPC.hs
@@ -1,14 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 module UTPC where
 
-import Web.Scotty (scotty)
+import Control.Monad.IO.Class
+
+import Network.HTTP.Types.Status
+import Network.Wai.Middleware.RequestLogger
+import Hackerrank
+import Web.Scotty
+
+-- |Take in a post request and either throw an error or redirect to the submissions page
+submit :: ActionM ()
+submit = do
+  flist <- files
+  case flist of
+    x:[] -> redirect "submit"
+    _    -> status status400
+
+languages :: ActionM ()
+languages = do
+    r <- liftIO Hackerrank.languages
+    json r
 
 -- |List of routes for the web application
 routes :: ScottyM ()
 routes = do
-  get "/"       (text "homepage")
+  get  "/"          (text "homepage")
+  get  "/languages" UTPC.languages
+  get  "/submit"    (text "submitted")
+  post "/submit"    submit
 
 main :: IO ()
 main = do
   putStrLn "Starting Server"
-  scotty 3000 routes
+  scotty 3000 $ middleware logStdoutDev >> routes
